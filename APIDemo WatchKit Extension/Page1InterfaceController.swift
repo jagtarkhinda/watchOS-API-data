d70c9b9464defab92e8bfa797ec6aaad2e934a30
MacStudent <macstudent@130-IMAC-01.local>
2019-06-26 10:08:24 -0400

    initial commit with tabel view

---
+++ APIDemo WatchKit Extension/Page1InterfaceController.swift
@@ -0,0 +1,19 @@
+//
+//  Page1InterfaceController.swift
+//  APIDemo WatchKit Extension
+//
+//  Created by MacStudent on 2019-06-26.
+//  Copyright © 2019 Parrot. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class PAGE1InterfaceController: WKInterfaceController {
+    
+    //outlet for page1 table
+    @IBOutlet var tableView: WKInterfaceTable!
+    
+}
---
+++ APIDemo WatchKit Extension/RowController.swift
@@ -0,0 +1,16 @@
+//
+//  RowController.swift
+//  APIDemo WatchKit Extension
+//
+//  Created by MacStudent on 2019-06-26.
+//  Copyright © 2019 Parrot. All rights reserved.
+//
+
+import WatchKit
+
+class RowController: NSObject{
+    
+    //label from page1 row
+    @IBOutlet var outputLabel: WKInterfaceLabel!
+    
+}
