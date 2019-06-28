b7b5519f8de322f6cc2e40c35e7d3f71af086f08
Jagtar Singh <jagtarsingh@Jagtars-Mac.local>
2019-06-28 10:23:42 -0700

    activity 2

--- APIDemo WatchKit Extension/Page1InterfaceController.swift
+++ APIDemo WatchKit Extension/Page1InterfaceController.swift
@@ -17,42 +17,29 @@ class PAGE1InterfaceController: WKInterfaceController,  WCSessionDelegate {
         
     }
     
+    // 1: Session property
+    private var session = WCSession.default
     
+    @IBOutlet var getMessage: WKInterfaceLabel!
+    @IBOutlet var sendMessage: WKInterfaceButton!
+    
+    @IBOutlet var R1Label: WKInterfaceLabel!
+    
+    
+    //Requirement 1, receiving message from phone
     func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
-        // Play a "click" sound when you get the message
-        WKInterfaceDevice().play(.click)
         
-        // output a debug message to the terminal
-        print("Got a message!")
-        
-        // update the message with a label
-        self.personList = [
-            message["firstname"] as! String,
-            message["lastname"] as! String
-        ]
-        
-       
-        self.tableView.setNumberOfRows(
-            self.personList.count, withRowType:"myrow"
-        )
-        
-        // 1. Tell watch what data goes in each row
-        // blue = outlet for your label
-        // pink = name of your data source
-        // yellow = name of custom row controller
-        for (index, country) in self.personList.enumerated() {
-            let row = self.tableView.rowController(at: index) as! RowController
-            row.outputLabel.setText(country)
-        }
+        R1Label.setText(message["Key"] as? String)
     }
+    
+
+    
     
     //outlet for page1 table
     @IBOutlet var tableView: WKInterfaceTable!
     
     
-    // MARK: Data Source
-    
-    var personList = ["jagtar", "jsk@hmilcom","66.8","556.8","45343","youy","12234"]
+
     
     
     override func awake(withContext context: Any?) {
@@ -74,19 +61,27 @@ class PAGE1InterfaceController: WKInterfaceController,  WCSessionDelegate {
         // blue = name of table outlet
         // pink = name of your data source
         // yellow = id for your row
-        self.tableView.setNumberOfRows(
-            self.personList.count, withRowType:"myrow"
-        )
-        
-        // 1. Tell watch what data goes in each row
-        // blue = outlet for your label
-        // pink = name of your data source
-        // yellow = name of custom row controller
-        for (index, country) in self.personList.enumerated() {
-            let row = self.tableView.rowController(at: index) as! RowController
-            row.outputLabel.setText(country)
-        }
+       
         
     }
     
+    //Requirement 2 , sending message to phone and receiving an ACK in return
+    @IBAction func pushMessage() {
+        
+        let mainMessage = ["Value":"Hello from Watch"]
+       print("hello hello")
+         if (WCSession.default.isReachable) {
+        session.sendMessage(mainMessage,
+                        replyHandler:
+            { replyMessage in
+            //handle and present the message on screen
+            let value = replyMessage["Value"] as? String
+            self.getMessage.setText(value)
+        }, errorHandler: {error in
+            // catch any errors here
+            print("error received is \(error)")
+        })
+    }
+    
+    }
 }
--- APIDemo/ViewController.swift
+++ APIDemo/ViewController.swift
@@ -14,6 +14,8 @@ import WatchConnectivity
 
 class ViewController: UIViewController, WCSessionDelegate {
     
+    var session: WCSession!
+    
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
 
     }
@@ -25,85 +27,20 @@ class ViewController: UIViewController, WCSessionDelegate {
     func sessionDidDeactivate(_ session: WCSession) {
 
     }
-
-
-    var datalist:[String : Any]?
-    @IBOutlet weak var outputLabel: UILabel!
     
-    @IBOutlet weak var errorchecking: UILabel!
     
+    @IBOutlet weak var getMessage: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        //let URL = "https://httpbin.org/get"
-        let URL = "https://randomuser.me/api/"
-        
-        Alamofire.request(URL).responseJSON {
-            
-            response in
-            
-            // TODO: Put your code in here
-            // ------------------------------------------
-            // 1. Convert the API response to a JSON object
-            
-            // -- check for errors
-            let apiData = response.result.value
-            if (apiData == nil) {
-                self.print("Error when getting API data")
-                return
-            }
-            // -- if no errors, then keep going
-            
-            self.print(apiData)
-            
-            
-            // 2. Parse out the data you need (sunrise / sunset time)
-            
-            // example2 - parse an array of dictionaries
-            
-            // 2a. Convert the response to a JSON object
-            let jsonResponse = JSON(apiData)
-            
-            self.print(jsonResponse)
-            
-            self.datalist = [
-                "firstname":  jsonResponse["results"][0]["name"]["first"].stringValue,
-                "lastname":  jsonResponse["results"][0]["name"]["last"].stringValue
-            ]
-            
-//            // 2b. Get the array out of the JSON object
-//            var responseArray = jsonResponse.arrayValue
-//
-//            // 2c. Get the 3rd item in the array
-//            // item #3 = position 2
-//            var item = responseArray[2];
-//            self.print(item)
-//
-//            // Output the "title" of the item in position #2
-//            self.outputLabel.text = item["title"].stringValue
-//
-////            // 2b. Get a key from the JSON object
-////            let origin = jsonResponse["origin"]
-////            let host = jsonResponse["headers"]["Host"]
-////
-////            // 2c. Output the value to screen
-////            print("Your IP Address: \(origin)")
-////            print("Host: \(host)")
-////
-////            // 3. Show the data in the user interface
-////            self.outputLabel.text = "IP Address: \(origin)"
-        }
-    
-        
         if (WCSession.isSupported()) {
-            errorchecking.text = ("Yes it is!")
+            
             let session = WCSession.default
             session.delegate = self
             session.activate()
         }
         else{
-            errorchecking.text = ("Phone can not connect to the watch")
+           getMessage.text = ("Phone can not connect to the watch")
         }
     }
 
@@ -113,32 +50,24 @@ class ViewController: UIViewController, WCSessionDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    //button on main phone screen
-    @IBAction func print(_ sender: Any) {
+    //Requirement 1, sending message to watch
+    @IBAction func sendFirstMessage(_ sender: Any) {
         
-        //if session is suppported then sendthe data to phone
-        if (WCSession.default.isReachable) {
-            // construct the message you want to send
-            // the message is in dictionary
-//            let message =
-//                ["message": "Hello",
-//                 "email": "jj@gmail.com"
-//            ]
-            errorchecking.text = "mesage sent"
-            // send the message to the watch
-            WCSession.default.sendMessage(datalist!, replyHandler: nil)
-        }
-        else{
-            errorchecking.text = "cannnot send the data to watch"
-        }
-        
-        //some pictures on phone
-
+        let msg = ["Key": "msg from fone"]
+         WCSession.default.sendMessage(msg, replyHandler: nil)
     }
     
-   
+    
+    //Requirement 2, receiving message from watch and sending the response back
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+        //handle received message
+        let value = message["Value"] as? String
+        DispatchQueue.main.async{
+            self.getMessage.text = value
+        }
+        //send a reply
+        replyHandler(["Value": "Got the message"])
+    }
+    }
 
-
-}
 
